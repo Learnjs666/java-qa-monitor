478f811cbb02b8c2646a20d96ad8c21ffe600025
kebi <js6zy666@gmail.com>
2026-03-11 22:48:01 +0800

    chore: init spring boot project and database ddl

---
+++ src/main/java/cn/edu/swpu/java_qa_monitor/JavaQaMonitorApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.swpu.java_qa_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQaMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaQaMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/swpu/java_qa_monitor/JavaQaMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.swpu.java_qa_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQaMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
